9b6f5fb6b47b25e154be7cbeac42ff68f08873dd
Tamás Rigó <rigo.tamas.seb@gmail.com>
2023-06-29 10:02:04 +0200

    add response mail

--- netlify/functions/email/email.ts
+++ netlify/functions/email/email.ts
@@ -12,8 +12,10 @@ export const handler: Handler = async (event, context) => {
     }
   }
   try {
-    const body = JSON.parse(event.body || '{}')
-    JSON.parse('')
+    if (!event.body) {
+      throw new Error('No body found in request!')
+    }
+    const body = JSON.parse(event.body)
     const transporter = nodemailer.createTransport({
       service: 'Gmail',
       auth: {
@@ -37,6 +39,26 @@ ${body.message.replaceAll('\n', '<br>')}`,
     }
     await transporter.sendMail(mailOptions);
     console.log(`email succesfully sent, name: ${body.name}, email: ${body.email}, phone: ${body.phone}`)
+    if (body.email) {
+
+      const responseMailOptions: SendMailOptions = {
+        from: 'menerke@gmail.com',
+        to: body.email,
+        subject: 'Megkeresés befogadva',
+        attachments: [
+          {
+            path: 'https://www.telikert-teraszbeepites.hu/wp-content/uploads/2015/12/brd_logo_400px.png',
+            filename: 'beridoor_logo.png'
+          }],
+        html: `<h2>Kedves ${body.name}</h2>
+<img src="beridoor_logo.png">
+<p>Megkeresését rendszerünk befogadta, munkatársunk hamarosan felveszi önnel a kapcsolatot.</p>
+<p>Üdvözlettel, Beridoor kft.</p>`,
+      }
+      transporter.sendMail(responseMailOptions)
+        .then(() => { console.log(`response email sent succesfully to ${body.email}`) })
+        .catch((e) => { console.log(`error sending response email to ${body.email}. Reason: ${e}`) });
+    }
     return {
       statusCode: 200,
       body: JSON.stringify({
